5c4136e2661f17b80ec1a2bea99900b98cbd787a
k-kinoshita <kinoken5939@gmail.com>
2021-08-11 18:39:14 +0900

    セクション5 組み込みメニューコマンドを追加する

---
+++ Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by 木下健一 on 2021/08/11.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
