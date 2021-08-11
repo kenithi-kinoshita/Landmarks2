a22fa175273761836d14af85bc088511266a37fd
k-kinoshita <kinoken5939@gmail.com>
2021-08-11 16:24:26 +0900

    watch 導入まで終了 動作確認済

---
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by 木下健一 on 2021/08/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
---
+++ Landmarks/WatchLandmarks Extension/LandmarksApp.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarksApp.swift
+//  WatchLandmarks Extension
+//
+//  Created by 木下健一 on 2021/08/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+    }
+}
